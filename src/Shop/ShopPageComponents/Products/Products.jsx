be71bf0fa91315c348ac108b884ed266dd945ea5
haiseno <haise.zhao@gmail.com>
2025-12-25 20:11:29 -0800

    Add search filtering
    Search with no subcategories selected will search
    from API endpoint.
    
    Search with subcategories selected will filter
    from existing selected subcategories.
    
    Add debounce and abort controller to ensure minimal
    fetch requests, not on every keystroke, and to
    prevent race conditions.

--- src/Shop/ShopPageComponents/Products/Products.jsx
+++ src/Shop/ShopPageComponents/Products/Products.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import Card from "./Card/Card.jsx";
 import styles from "./Products.module.css";
 
 export default function Products() {
   const { products, setProducts, filters, searchValue } = useOutletContext();
+  const [searchProducts, setSearchProducts] = useState([]);
 
   const cachedSubcategories = Object.keys(products);
   const subCategories = Object.values(filters.subCategories).flat();
@@ -39,9 +40,54 @@ export default function Products() {
     fetchProducts();
   }, [filters.subCategories]);
 
+  useEffect(() => {
+    if (subCategories.length || !searchValue.trim()) {
+      return;
+    }
+
+    const controller = new AbortController();
+
+    const search = searchValue.trim();
+
+    const fetchTimeout = setTimeout(() => {
+      async function fetchProducts() {
+        try {
+          const response = await fetch(
+            `https://dummyjson.com/products/search?q=${encodeURIComponent(search)}`,
+            {
+              mode: "cors",
+              signal: controller.signal,
+            }
+          );
+
+          if (!response.ok) {
+            throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+          }
+
+          const data = await response.json();
+
+          setSearchProducts(data.products);
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      }
+
+      fetchProducts();
+    }, 300);
+
+    return () => {
+      clearTimeout(fetchTimeout);
+      controller.abort();
+    };
+  }, [searchValue, filters.subCategories]);
+
   function getListOfProducts() {
-    const productsList = subCategories.flatMap((subCategory) => products[subCategory] ?? []);
-    const normalizeSearch = searchValue.toLowerCase();
+    const productsList =
+      subCategories.length > 0
+        ? subCategories.flatMap((subCategory) => products[subCategory] ?? [])
+        : searchProducts;
+
+    const normalizeSearch = searchValue.trim().toLowerCase();
 
     if (normalizeSearch) {
       return productsList.filter((product) => {
@@ -57,7 +103,7 @@ export default function Products() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Products</h1>
-      {subCategories.length === 0 ? (
+      {subCategories.length === 0 && searchProducts.length === 0 ? (
         <h2>Choose a category or search to view products.</h2>
       ) : (
         <ul className={styles.gridContainer}>
